9f4c01beb630d507e405c9a6cae515eb7dacb82a
Sven Rademakers <sven.rademakers@gmail.com>
2023-01-20 18:03:43 +0100

    Experimentation phase. experimented with loom a bit. First step is to focus on the implementation in a single threaded context.

--- src/binarytree.rs
+++ src/binarytree.rs
@@ -1,67 +1,158 @@
-use loom::{
-    sync::atomic::{AtomicUsize, Ordering},
+use std::ops::Deref;
+use std::{
+    sync::atomic::{AtomicPtr, AtomicUsize, Ordering},
     sync::Arc,
     thread,
 };
-use std::ops::Deref;
 
-/// This object operates on an homogeneous piece of memory.
-/// as one would expect, this object is threadsafe. And is written for learning purposes
-/// Internally it uses an binary tree structure, this means you can expect :
-/// * insert O=Log(n)
-/// * pop O= 1
-///
-pub struct LockFreeBinaryTree<T, const N: usize> {
-    storage: [T; N],
-    head: AtomicUsize,
+/// Part of the [LockFreeBinaryTree]. Used to build the binary tree with
+/// TODO: we need to have a thread-safe reference count somewhere to make sure
+/// that a thread cannot modify the pointers while others are reading them.
+struct Node<T> {
+    pub data: AtomicPtr<T>,
+    pub left: AtomicPtr<Node<T>>,
+    pub right: AtomicPtr<Node<T>>,
 }
 
-//unsafe impl<T, const N: usize> Sync for LockFreeBinaryTree<T, N> {
-//}
+impl<T> Node<T> {
+    pub fn leaky<U: Into<Box<T>>>(item: U) -> *mut Self {
+        let left = AtomicPtr::new(std::ptr::null_mut::<Node<T>>());
+        let right = AtomicPtr::new(std::ptr::null_mut::<Node<T>>());
 
-impl<T: Default + Copy + Sync + Send + std::fmt::Debug, const N: usize> LockFreeBinaryTree<T, N> {
+        let raw = Box::into_raw(item.into());
+        let boxed = Box::from(Self {
+            data: AtomicPtr::new(raw),
+            left,
+            right,
+        });
+
+        Box::into_raw(boxed)
+    }
+}
+
+/// structure that guards the immutability lifetime of the contained node. As long an instance of
+/// this guard exists no other threads can modify the node.
+pub struct DerefGuard<'a, T> {
+    phantom: std::marker::PhantomData<&'a T>,
+    node: *const Node<T>,
+}
+
+impl<'a, T> DerefGuard<'a, T> {
+    fn new(node: *const Node<T>) -> Self {
+        Self {
+            phantom: std::marker::PhantomData::default(),
+            node,
+        }
+    }
+}
+
+impl<'a, T> Deref for DerefGuard<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*(*self.node).data.load(Ordering::Acquire) }
+    }
+}
+
+pub struct LockFreeBinaryTree<T> {
+    head: AtomicPtr<Node<T>>,
+}
+
+impl<T: Send + std::cmp::Ord> LockFreeBinaryTree<T> {
     pub fn new() -> Self {
         Self {
-            storage: [T::default(); N],
-            head: AtomicUsize::new(0),
+            head: AtomicPtr::new(std::ptr::null_mut()),
         }
     }
 
-    fn mut_ptr(&self) -> *mut [T; N] {
-        &self.storage as *const _ as *mut _
-    }
-
-    fn as_ptr(&self) -> *const [T; N] {
-        &self.storage as *const _ 
-    }
-
-    pub fn push(&self, val: T) -> bool {
-        let index = self.head.fetch_add(1, Ordering::Relaxed);
-        if index >= N {
-            // no more space left!
-            return false;
-        }
-
-        unsafe {
-            let storage = &mut *self.mut_ptr();
-            storage[index] = val;
+    pub fn push<U: Into<Box<T>>>(&self, item: U) -> bool {
+        let new_node = Node::leaky(item);
+        loop {
+            let current = self.head.load(Ordering::Relaxed);
+            if current.is_null() {
+                match self.head.compare_exchange(
+                    current,
+                    new_node,
+                    Ordering::Release,
+                    Ordering::Relaxed,
+                ) {
+                    Ok(_) => break,
+                    //other thread was quicker, proceed with adding it
+                    Err(_) => continue,
+                }
+            } else {
+                unsafe {
+                    let data = (&*new_node).data.load(Ordering::Acquire);
+                    let _found_node = self.relaxed_search(current, &*data);
+                    todo!();
+                }
+            }
         }
         return true;
     }
 
-    pub fn pop(&self) -> Option<T> {
-        // self.head.fetch_max()
-        // head can be empty, if we are in push operation
-        todo!();
+    /// Find the given element in the container and return a thread-safe
+    /// immutable reference to this element, If the item does not exist, return
+    /// the item closest.
+    ///
+    /// # Arguments
+    ///
+    /// * 'item'    item to find in the container
+    ///
+    /// # Returns
+    ///
+    /// A immutable reference to the found item wrapped in a guard. This guard
+    /// makes sure no writes happen as long as the guard object exists.
+    pub fn find<'a>(&self, item: &T) -> DerefGuard<'a, T> {
+        let (node, _) = self.relaxed_search(self.head.load(Ordering::Acquire), item);
+        DerefGuard::new(node)
+    }
+
+    pub fn delete(&self, item: &T) -> bool {
+        todo!()
+    }
+
+    /// This function walks over the tree to find the node that is closest to the
+    /// given value. Note: The tree can get modified while we are walking over it. 
+    ///
+    /// # Arguments
+    ///
+    /// * 'current' the root node to start searching with. Cannot be null!
+    /// * 'value' the value to find
+    ///
+    /// # Return
+    ///
+    /// Pointer to the value closest to the given value.
+    fn relaxed_search(&self, current: *const Node<T>, to_find: &T) -> (*const Node<T>, std::cmp::Ordering) {
+        assert!(!current.is_null());
+
+        unsafe {
+            let node_data = (*current).data.load(Ordering::Acquire);
+            // data pointers cannot be invalid!
+            assert!(!node_data.is_null());
+
+            let ordering = (&*node_data).cmp(to_find);
+            let new_current = match ordering {
+                std::cmp::Ordering::Less => (*current).left.load(Ordering::Relaxed),
+                std::cmp::Ordering::Greater => (*current).right.load(Ordering::Relaxed),
+                std::cmp::Ordering::Equal => std::ptr::null_mut(),
+            };
+
+            if new_current.is_null() {
+                return (current, ordering);
+            }
+
+        (new_current, ordering)
+        }
     }
 }
 
 #[test]
 fn test_push() {
     loom::model(|| {
-        const thread_n: usize = loom::MAX_THREADS - 1;
-        let queue: Arc<LockFreeBinaryTree<usize, thread_n>> = Arc::new(LockFreeBinaryTree::new());
-        let threads: Vec<_> = (0..thread_n)
+        const THREAD_N: usize = loom::MAX_THREADS - 1;
+        let queue: Arc<LockFreeBinaryTree<usize>> = Arc::new(LockFreeBinaryTree::new());
+        let threads: Vec<_> = (0..THREAD_N)
             .map(|n| {
                 let clone = queue.clone();
                 thread::spawn(move || {
@@ -73,9 +164,15 @@ fn test_push() {
         for handle in threads {
             handle.join().unwrap();
         }
-
-        unsafe {
-            let storage = &*queue.deref().mut_ptr();
-        }
     });
+}
+
+#[test]
+fn push_test() {
+    let tree = LockFreeBinaryTree::<usize>::new();
+    assert!(tree.push(123));
+    unsafe {
+        let head = &*tree.head.load(Ordering::Relaxed);
+        assert_eq!(123, *head.data.load(Ordering::Relaxed));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
+//! This crate provides containers that are lock free. Please note that these
+//! containers might underperform in highly concurrent environments. Combined
+//! with the fact that this crate is made for educational purposes, makes this
+//! crate not a good candidate for usage in production.
 pub mod binarytree;
-
